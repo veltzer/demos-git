92e4c512a5519ede0d2cf13ab84db9a981f1df74
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-12 14:38:39 +0200

    script

---
+++ scripts/same_time_commit_of_same_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -rf example1 example2
+git init example1
+git init example2
+cd example1
+echo hello > hello.txt
+git add hello.txt
+cd ../example2
+echo hello > hello.txt
+git add hello.txt
+
+# we you ready?
+git commit -m "first commit"
+cd ../example1
+git commit -m "first commit"
