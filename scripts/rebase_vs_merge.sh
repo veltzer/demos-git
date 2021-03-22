17b840bc39162f85f5e78829a88a0ab380c7552c
Mark Veltzer <mark.veltzer@gmail.com>
2021-03-23 01:48:19 +0200

    better rebase vs merge

--- scripts/rebase_vs_merge.sh
+++ scripts/rebase_vs_merge.sh
@@ -2,67 +2,101 @@
 
 source common.sh
 
-rm -rf server user1 user2
-git init --bare server
-git clone server user1
-git clone server user2
-cd user1
-touch hello.txt
-git add hello.txt
-git commit -m "first commit"
-git push
-cd ../user2
-git pull
-cd ../user1
-echo "history for user1..."
-git log --pretty=format:"%h %s" --graph
-cd ../user2
-echo "history for user2..."
-git log --pretty=format:"%h %s" --graph
-echo "histories above should be the same..."
-echo "press any key..."
-waitkey
-cd ../user1
-for i in {1..5}; do touch user1change$i.txt; git add . ; git commit -m "user1 change $i"; done
-cd ../user2
-for i in {1..3}; do touch user2change$i.txt; git add . ; git commit -m "user2 change $i"; done
+debug=false
 
-cd ../user1
-echo "history for user1..."
-git log --pretty=format:"%h %s" --graph
-cd ../user2
-echo "history for user2..."
-git log --pretty=format:"%h %s" --graph
-echo "histories above are different..."
-echo "press any key..."
-waitkey
+function debug() {
+	local msg=$1
+	if $debug
+	then
+		echo $msg
+	fi
+}
 
-echo "user1 pushed..."
-cd ../user1
-git push
-echo "press any key..."
-waitkey
+function doit() {
+	local rebase=$1
+	rm -rf server user1 user2
+	git init --bare server
+	git clone server user1
+	git clone server user2
+	cd user1
+	touch hello.txt
+	git add hello.txt
+	git commit -m "first commit"
+	git push
+	cd ../user2
+	git pull
+	cd ../user1
+	debug "history for user1..."
+	git log --pretty=format:"%h %s" --graph
+	cd ../user2
+	debug "history for user2..."
+	git log --pretty=format:"%h %s" --graph
+	debug "histories above should be the same..."
+	debug "press any key..."
+	waitkey
+	cd ../user1
+	for i in {1..5}; do touch user1change$i.txt; git add . ; git commit -m "user1 change $i"; done
+	cd ../user2
+	for i in {1..3}; do touch user2change$i.txt; git add . ; git commit -m "user2 change $i"; done
 
-echo "user2 cannot push..."
-cd ../user2
-git push
-echo "press any key..."
-waitkey
+	cd ../user1
+	debug "history for user1..."
+	git log --pretty=format:"%h %s" --graph
+	cd ../user2
+	debug "history for user2..."
+	git log --pretty=format:"%h %s" --graph
+	debug "histories above are different..."
+	debug "press any key..."
+	waitkey
 
-echo "user2 pulls..."
-git pull --rebase
-echo "user2 pushes..."
-git push
-echo "user1 pulls..."
-cd ../user1
-git pull
+	debug "user1 pushed..."
+	cd ../user1
+	git push
+	debug "press any key..."
+	waitkey
 
-echo "recording logs for posterity..."
-echo "user1"
-git log --pretty=format:"%h %s" --graph
-echo "user2"
-cd ../user2
-git log --pretty=format:"%h %s" --graph
+	debug "user2 cannot push..."
+	cd ../user2
+	git push
+	debug "press any key..."
+	waitkey
 
-cd ..
-rm -rf server user1 user2
+	debug "user2 pulls..."
+	if $rebase
+	then
+		git pull --rebase=true --no-edit
+	else
+		git pull --rebase=false --no-edit
+	fi
+	debug "user2 pushes..."
+	git push
+	debug "user1 pulls..."
+	cd ../user1
+	git pull
+
+	debug "recording logs for posterity..."
+	debug "user1"
+	git log --pretty=format:"%h %s" --graph > /tmp/log1_$rebase
+	debug "user2"
+	cd ../user2
+	git log --pretty=format:"%h %s" --graph > /tmp/log2_$rebase
+
+	diff /tmp/log1_$rebase /tmp/log2_$rebase
+
+	cd ..
+	rm -rf server user1 user2
+}
+
+debug merge
+doit false
+debug rebase
+doit true
+
+# show results
+echo
+echo "merge log"
+cat /tmp/log1_false
+echo
+echo "rebase log"
+cat /tmp/log1_true
+echo
