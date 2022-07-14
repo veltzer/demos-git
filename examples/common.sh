cb2e7df88adc911483b71690a34a01bb03ad8a5b
Mark Veltzer <mark.veltzer@gmail.com>
2022-07-14 19:19:33 +0300

    started big fixup

--- examples/common.sh
+++ examples/common.sh
@@ -1,25 +1,50 @@
-function git_common_commits() {
+function common_cleanup() {
+	rm -rf playground/*
+}
+
+function common_create_commits() {
 	local number=$1
 	local name=$2
 	let x=1
 	while [[ $x -le ${number} ]]
 	do
-		touch ${name}_${x}
-		git add ${name}_${x}
+		touch "${name}_${x}"
+		git add "${name}_${x}"
 		git commit -m "${name} commit ${x}"
 		let "x=x+1"
 	done
 }
 
-function git_common_repo_with_several_commits() {
-	rm -rf repo
-	git init repo
-	cd repo
-	commits 10 "just another"
-	cd ../
+function common_show_history() {
+	local repo=$1
+	local folder="playground/${repo}"
+	cd "${folder}"
+	git log --pretty=format:"%h %s" --graph
+	cd ../../
 }
 
-function git_common_server_two_users() {
+function common_repo_with_one_commit() {
+	local repo=$1
+	local name=$2
+	local folder="playground/${repo}"
+	git init "${folder}"
+	cd "${folder}"
+	common_create_commits 1 $name
+	cd ../../
+}
+
+function common_create_repo_with_commits() {
+	local repo=$1
+	local number=$2
+	local name=$3
+	local folder="playground/${repo}"
+	git init "${folder}"
+	cd "${folder}"
+	common_create_commits $number $name
+	cd ../../
+}
+
+function common_server_two_users() {
 	rm -rf server user?
 	git init --bare server
 	git clone server user1
@@ -36,24 +61,16 @@ function git_common_server_two_users() {
 
 do_wait=0
 
-function git_common_waitkey() {
+function common_waitkey() {
 	if [[ $do_wait -eq 1 ]]
 	then
 		read
 	fi
 }
 
-function debug() {
-	local msg=$1
-	if $debug
-	then
-		echo $msg
-	fi
-}
-
 debug=false
 
-function debug() {
+function common_debug() {
 	local msg=$1
 	if $debug
 	then
--- examples/same_time_commit_of_same_data.sh
+++ examples/same_time_commit_of_same_data.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-rm -rf example1 example2
-git init example1
-git init example2
-cd example1
-echo hello > hello.txt
-git add hello.txt
-cd ../example2
-echo hello > hello.txt
-git add hello.txt
 
-# we you ready?
-git commit -m "first commit"
-cd ../example1
-git commit -m "first commit"
+source common.sh
 
-# lets show the git log of the two project
-git log
-cd ../example2
-git log
+common_cleanup
+common_repo_with_one_commit repo1 "first commit"
+common_repo_with_one_commit repo2 "first commit"
+echo "notice that these commits, from two different repos, have the same id!"
+common_show_history repo1
+common_show_history repo2
+common_cleanup
