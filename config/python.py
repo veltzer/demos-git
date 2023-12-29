4533319f60b5ebe416d0cf36d4ecaa5d4659019c
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 01:52:13 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,6 +1,9 @@
-config_requires = []
-dev_requires = []
-install_requires = []
-make_requires = []
-test_requires = []
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = []
+install_requires: List[str] = []
+make_requires: List[str] = []
+test_requires: List[str] = []
 requires = config_requires + install_requires + make_requires + test_requires
