9a14b6df2546cc902180cd616043dbc403d04028
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-12 16:20:31 +0300

    no commit message given

--- examples/bisect.sh
+++ examples/bisect.sh
@@ -6,4 +6,20 @@ source common.sh
 
 common_setup
 common_create_repo demo
+common_cd_repo demo
+cat << EOF > test.sh
+#!/bin/bash
+
+test ! -f commit_57
+EOF
+chmod +x ./test.sh
+git add test.sh
+git commit -m "first commit"
+common_cd_repo_back demo
+common_create_commits demo 100 commit
+common_cd_repo demo
+git bisect start HEAD HEAD~100
+git bisect run "./test.sh"
+common_cd_repo_back demo
+# common_show_history demo
 common_finish
